6c963420c62cfd15801f3f7ebd429494f2161dca
hbl917070 <hbl917070@hotmail.com.tw>
2022-07-28 07:24:33 +0800

    [add] 專案初始版本

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tiefsee4.QuickLook
+
+此專案為 [Tiefsee4](https://github.com/hbl917070/Tiefsee4) 的擴充套件，<br>
+在桌面或資料夾時，長按空白鍵即可透過 Tiefsee 來預覽檔案。
+<br><br>
+程式碼來自：https://github.com/QL-Win/QuickLook
+
+<br><hr><br>
+
+This project is a Plugin for [Tiefsee4](https://github.com/hbl917070/Tiefsee4),<br>
+When you are on the desktop or in a folder, press and hold the blank key to preview the file through Tiefsee.
+<br><br>
+The code comes from：https://github.com/QL-Win/QuickLook
---
+++ Tiefsee.QuickLook/QuickLook.cs
@@ -0,0 +1,118 @@
+﻿// Copyright © 2017 Paddy Xu
+// 
+// This file is part of QuickLook program.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiefsee {
+    public class QuickLook {
+        private const int MaxPath = 32767;
+
+        static bool Is64Bit = Environment.Is64BitProcess;
+
+        [DllImport("QuickLook.Native32.dll", EntryPoint = "Init", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void Init_32();
+
+        [DllImport("QuickLook.Native32.dll", EntryPoint = "GetFocusedWindowType", CallingConvention = CallingConvention.Cdecl)]
+        private static extern FocusedWindowType GetFocusedWindowTypeNative_32();
+
+        [DllImport("QuickLook.Native32.dll", EntryPoint = "GetCurrentSelection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void GetCurrentSelectionNative_32([MarshalAs(UnmanagedType.LPWStr)] StringBuilder sb);
+
+        [DllImport("QuickLook.Native64.dll", EntryPoint = "Init", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void Init_64();
+
+        [DllImport("QuickLook.Native64.dll", EntryPoint = "GetFocusedWindowType", CallingConvention = CallingConvention.Cdecl)]
+        private static extern FocusedWindowType GetFocusedWindowTypeNative_64();
+
+        [DllImport("QuickLook.Native64.dll", EntryPoint = "GetCurrentSelection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void GetCurrentSelectionNative_64([MarshalAs(UnmanagedType.LPWStr)] StringBuilder sb);
+
+        internal static void Init() {
+            try {
+                if (Is64Bit)
+                    Init_64();
+                else
+                    Init_32();
+            } catch (Exception e) {
+                Debug.WriteLine(e);
+            }
+        }
+
+        internal static FocusedWindowType GetFocusedWindowType() {
+            try {
+                return Is64Bit ? GetFocusedWindowTypeNative_64() : GetFocusedWindowTypeNative_32();
+            } catch (Exception e) {
+                Debug.WriteLine(e);
+                return FocusedWindowType.Invalid;
+            }
+        }
+
+        /// <summary>
+        /// 取得目前焦點檔案總管或桌面選取的檔案，如果焦點不在桌面或未選取檔案，則返回 ""
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCurrentSelection() {
+            if (QuickLook.GetFocusedWindowType() == QuickLook.FocusedWindowType.Invalid) {
+                return "";
+            }
+
+            StringBuilder sb = null;
+            try {
+                // communicate with COM in a separate thread
+                Task.Run(() => {
+                    sb = new StringBuilder(MaxPath);
+                    if (Is64Bit)
+                        GetCurrentSelectionNative_64(sb);
+                    else
+                        GetCurrentSelectionNative_32(sb);
+                }).Wait();
+            } catch (Exception e) {
+                Debug.WriteLine(e);
+            }
+
+            return ResolveShortcut(sb?.ToString() ?? string.Empty);
+        }
+
+        private static string ResolveShortcut(string path) {
+            if (string.IsNullOrEmpty(path)) return path;
+
+            if (Path.GetExtension(path).ToLower() != ".lnk") return path;
+
+            var link = new ShellLink();
+            ((IPersistFile)link).Load(path, 0);
+            var sb = new StringBuilder(MaxPath);
+            ((IShellLinkW)link).GetPath(sb, sb.Capacity, out _, 0);
+
+            return sb.Length == 0 ? path : sb.ToString();
+        }
+
+        internal enum FocusedWindowType {
+            Invalid,
+            Desktop,
+            Explorer,
+            Dialog,
+            Everything,
+            DOpus
+        }
+    }
+}
